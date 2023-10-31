e53217aebb0926fb6fa02045da33a48a5e2f4327
uozok <uplosub@gmail.com>
2023-11-01 00:36:35 +0900

    chenge_settingpy.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xx)fl7=h=a$%kz_cvzlfeey0qfjnze%e*o67(m*!3wsvsu)-+-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', '192.168.111.8']
 
