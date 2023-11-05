a23beea6d07736a975174890ed87eb7449376b0d
uozok <uplosub@gmail.com>
2023-11-05 19:36:15 +0900

    Changed ui.

--- label_list/views.py
+++ label_list/views.py
@@ -18,11 +18,20 @@ def album_list(request):
         )
     return render(request, 'label_list/album_list.html', {'albums': albums, 'search_term': search_term})
 
-
 def album_detail(request, album_id):
     album = get_object_or_404(Album, id=album_id)
-    search_term = request.GET.get('search', '')  # 追加
-    return render(request, 'label_list/album_detail.html', {'album': album, 'search_term': search_term})  # search_termを追加
+    search_term = request.GET.get('search', '')
+    meta_title = f"{album.title} by {album.artist.artist if album.artist else 'Unknown Artist'}"
+    meta_description = f"{album.notes[:155]}..." if album.notes else "No description available."
+    meta_image = request.build_absolute_uri(album.image.url) if album.image else None
+    context = {
+        'album': album,
+        'search_term': search_term,
+        'meta_title': meta_title,
+        'meta_description': meta_description,
+        'meta_image': meta_image,
+    }
+    return render(request, 'label_list/album_detail.html', context)
 
 def news_list(request):
     news_posts = NewsPost.objects.filter(published_date__isnull=False).order_by('published_date')
